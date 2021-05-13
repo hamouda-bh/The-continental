96551bca681fe75b100ef78035a51171685275a1
Elamri Maissa <maissa.elamri@esprit.tn>
2021-05-13 23:49:11 +0100

    Model-routes-controllerPersonnel

--- config/db.config.js
+++ config/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     host: 				'localhost',
     user: 				'root',
-    password: 			'root',
+    password: 			'',
     database: 			'the_continental',
     waitForConnections: true
 };
